a5024a76a8156e9c4a4fb655035d6b5cfdbdc5e0
MonarchDev <miguelarellan117@gmail.com>
2025-07-05 21:18:04 -0600

    First app added

---
+++ FirstApp/app/src/main/java/com/monarch/androidmaster/firstapp/FirstAppActivity.kt
@@ -0,0 +1,32 @@
+package com.monarch.androidmaster.firstapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.monarch.androidmaster.R
+
+class FirstAppActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first_app)
+
+        val btnStart = findViewById<AppCompatButton>(R.id.btnStart) //En esta variable se linkea el button del archivo xml con el archivo kotlin
+        val etName = findViewById<AppCompatEditText>(R.id.etName)  //Aqui se linkea el editText del xml al kotlin
+
+        btnStart.setOnClickListener { //En esta linea el "setOnClickListener" sirve para ejecutar una acción cuando se hace un click.
+            val name = etName.text.toString()
+
+            if (name.isNotEmpty()){
+                val intent = Intent(this, ResultActivity::class.java)
+                intent.putExtra("EXTRA_NAME", name)
+                startActivity(intent)
+            }
+        }
+    }
+}
